e608d3b814e55653b1c6eb48d9c5b5a6ccfd4600
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-09-17 20:13:16 -0700

    Chapter 1 RAWR!

---
+++ thagomizer/ch01/ch01.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(+ 1 1)
+(+ 3 (* 2 4))
+(sqrt 9)
+(sqrt -9)
+(+ 1 2 3 4 5 6 7 8 9 0)
+(sqrt (+ (sqr 3) (sqr 4)))
+'((1 3 5 7 9) (2 4 6 8 0))
+'(sqrt (+ (sqr 3) (sqr 4)))
+'(hello world)
