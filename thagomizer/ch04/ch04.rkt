894be80428c31e3ec4165e712eaf8db0474b5d62
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-09-24 20:46:02 -0700

    Chapter 4  RAWR!

--- thagomizer/ch04/ch04.rkt
+++ thagomizer/ch04/ch04.rkt
@@ -126,4 +126,37 @@ is-it-even
 ;;(define filename "my-first-program.rkt")
 ;; (unless (ask-user-whether-to-keep-file filename)
 ;;   (delete-file filename))
-(and (even? x) (set! is-it-even #t))
+
+(if (member 4 (list 3 4 1 5)) '4-is-in 'not-in)
+(member 1 '(3 4 1 5))
+
+(define tasks '(1 clean 3 homework 4 party))
+(member 3 tasks)
+
+;; 44 Equality Predicates, Once More
+(struct point (x y) #:transparent)
+(define (distance-to-origin p)
+  (sqrt (+ (sqr (point-x p)) (sqr (point-y p)))))
+(distance-to-origin (point 3 4))
+(distance-to-origin (point 12 5))
+
+(define pt1 (point -1 2))
+(define pt2 (point -1 2))
+(equal? pt1 pt2)
+
+(eq? pt1 pt2)
+(eq? pt1 pt1)
+(eq? pt2 pt2)
+
+;; equal? is value equality. eq? is object-id equality.
+
+(define pt3 pt1)
+(eq? pt1 pt3)
+
+(define (eq-first-items list1 list2)
+  (eq? (first list1) (first list2)))
+
+(eq-first-items (cons pt1 empty) (cons pt3 empty))
+(eq-first-items (cons pt1 empty) (cons pt2 empty))
+
+;; 4.5 Comparing and Testing
