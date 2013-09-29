6e8b0d3923124e41f5085096a577c9dd20862d5d
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-09-29 16:09:37 -0700

    Added guess count
    Guess count is displayed in red to the left of the main number.

---
+++ thagomizer/ch05/medium_challenge.rkt
@@ -0,0 +1,69 @@
+#lang racket
+(require 2htdp/universe 2htdp/image)
+
+(struct interval (small big guess-count))
+
+(define TEXT-SIZE 14)
+(define HELP-TEXT
+  (text "↑ for larger numbers, ↓ for smaller ones"
+        TEXT-SIZE
+        "blue"))
+(define HELP-TEXT2
+  (text "Press = when your number is guessed; q to quit."
+        TEXT-SIZE
+        "blue"))
+(define WIDTH (+ (image-width HELP-TEXT2) 10))
+(define HEIGHT 150)
+(define COLOR "red")
+(define SIZE 72)
+(define COUNT-SIZE 36)
+(define TEXT-X 3)
+(define TEXT-UPPER-Y 10)
+(define TEXT-LOWER-Y 135)
+(define TEXT-MIDDLE-Y 62)
+
+(define (MT-SC w)
+  (place-image/align
+   HELP-TEXT TEXT-X TEXT-UPPER-Y "left" "top"
+   (place-image/align
+    (text (number->string (interval-guess-count w)) COUNT-SIZE COLOR) TEXT-X TEXT-MIDDLE-Y "left" "middle"
+    (place-image/align
+     HELP-TEXT2 TEXT-X TEXT-LOWER-Y "left" "bottom"
+     (empty-scene WIDTH HEIGHT)))))
+
+(define (deal-with-guess w key)
+  (cond [(key=? key "up") (bigger w)]
+        [(key=? key "down") (smaller w)]
+        [(key=? key "q") (stop-with w)]
+        [(key=? key "=") (stop-with w)]
+        [else w]))
+
+(define (smaller w)
+  (interval (interval-small w)
+            (max (interval-small w) (sub1 (guess w)))
+            (add1 (interval-guess-count w))))
+
+(define (bigger w)
+  (interval (min (interval-big w) (add1 (guess w)))
+            (interval-big w)
+            (add1 (interval-guess-count w))))
+
+(define (guess w)
+  (quotient (+ (interval-small w) (interval-big w)) 2))
+
+
+(define (render w)
+  (overlay (text (number->string (guess w)) SIZE COLOR)
+           (MT-SC w)))
+
+(define (single? w)
+  (= (interval-small w) (interval-big w)))
+
+(define (render-last-scene w)
+  (overlay (text "End" SIZE COLOR) (MT-SC w)))
+
+(define (start lower upper)
+  (big-bang (interval lower upper 0)
+            (on-key deal-with-guess)
+            (to-draw render)
+            (stop-when single? render-last-scene)))
