64220cfa77765943d1ecd2d5581217fd35930bf9
Ryan Davis <ryand-ruby@zenspider.com>
2013-10-01 16:48:57 -0700

    Hard: first stage, getting the UFO to wrap and respond to key commands.

--- zenspider/ch05/ufo.rkt
+++ zenspider/ch05/ufo.rkt
@@ -1,24 +1,56 @@
 #lang racket
 
 (require 2htdp/universe 2htdp/image)
+(require data/queue)
+
+(struct game (tick pos trail) #:mutable #:transparent)
+(struct pos (x y) #:transparent)
 
 (define WIDTH  640)
 (define HEIGHT 480)
-
-(define (add-3-to-state current-state)
-  (+ current-state 3))
-
-(define (draw-a-ufo-onto-an-empty-scene current-state)
-  (place-image
-   IMAGE-of-UFO (/ WIDTH 2) current-state
-   (empty-scene WIDTH HEIGHT)))
-
 (define IMAGE-of-UFO (bitmap/file "ch05/ufo.png"))
+(define PV 10)
+(define NV (- PV))
 
-(define (state-is-300 current-state)
-  (>= current-state 300))
+(define (new-game)
+  (game 0
+        (pos (/ WIDTH 2) (/ HEIGHT 2))
+        '()))
 
-(big-bang 0
-          [on-tick add-3-to-state]
-          [to-draw draw-a-ufo-onto-an-empty-scene]
-          [stop-when state-is-300])
+(define (tick w)
+  (set-game-tick! w (add1 (game-tick w)))
+  w)
+
+(define (draw-game-tick w)
+  (let ((p (game-pos w)))
+   (place-image/align
+    (text (format "~s x ~s : ~s" (pos-x p) (pos-y p) (game-tick w)) 12 "black")
+    10 10 'left 'top
+    (empty-scene WIDTH HEIGHT))))
+
+(define (draw-ufo w)
+  (let ((p (game-pos w)))
+    (place-image IMAGE-of-UFO (pos-x p) (pos-y p)
+                 (draw-game-tick w))))
+
+(define (move-ufo w x y)
+  (let ((p (game-pos w)))
+    (set-game-pos! w (pos (modulo (+ x (pos-x p)) WIDTH)
+                          (modulo (+ y (pos-y p)) HEIGHT)))
+    w))
+
+(define (key-handler w key)
+  (cond [(key=? key "q")      (stop-with w)]
+        [(key=? key "escape") (stop-with w)]
+
+        [(key=? key "up")     (move-ufo w 0 NV)]
+        [(key=? key "down")   (move-ufo w 0 PV)]
+        [(key=? key "left")   (move-ufo w NV 0)]
+        [(key=? key "right")  (move-ufo w PV 0)]
+
+        [else w]))
+
+(big-bang (new-game)
+          [on-tick tick]
+          [to-draw draw-ufo]
+          [on-key  key-handler])
