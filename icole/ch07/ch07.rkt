c40ea669ba5b0ffc0d4274e7f6e5e3b9a6b8cf91
icole <ian.cole@windermeresolutions.com>
2013-10-15 16:50:43 -0700

    Added missing parts to CH07

--- icole/ch07/ch07.rkt
+++ icole/ch07/ch07.rkt
@@ -18,3 +18,22 @@
         [(pred (first lst))
          (cons (first lst) (my-filter (rest lst)))]
         [else (my-filter (rest lst))]))
+
+(define (my-ormap pred lst)
+  (cond [(empty? lst) #f]
+        [else (or (pred (first lst))
+                  (my-ormap pred (rest lst)))]))
+
+(define (my-andmap pred lst)
+  (cond [(empty? lst) #t]
+        [else (and (pred (first lst))
+                   (my-andmap pred (rest lst)))]))
+
+(define (my-foldl f base lst)
+  (cond [(empty? lst) base]
+        [else (my-foldl f (f (first lst) base) (rest lst))]))
+
+(define (d/dx fun)
+  (define ∂ (/ 1 100000))
+  (lambda (x)
+    (/ (- (fun (+ x ∂)) (fun (- x ∂))) 2 ∂)))
