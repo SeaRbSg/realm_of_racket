bc6217e2b27b4c56462926f4106de0566f2319a7
Ben Lakey <benlakey@gmail.com>
2013-10-14 17:52:04 -0700

    Finished out snake from chapter 6

--- benlakey/ch06/snake.rkt
+++ benlakey/ch06/snake.rkt
@@ -31,9 +31,32 @@
 (struct point (x y) #:transparent)
 (struct goo (location expiration-ticks) #:transparent)
 
-;; to answer: does order matter? can use before define?
+;; control functions
+(define (direct-snake w key-event)
+  (cond [(dir? key-event) (world-change-dir w key-event)]
+        [else w]))
 
-;; assist functions
+(define (dir? x)
+      (or (key=? x "up")
+          (key=? x "down")
+          (key=? x "left")
+          (key=? x "right")))
+
+(define (opposite-dir? d1 d2)
+  (cond [(string=? d1 "up") (string=? d2 "down")]
+        [(string=? d1 "down") (string=? d2 "up")]
+        [(string=? d1 "left") (string=? d2 "right")]
+        [(string=? d1 "right") (string=? d2 "left")]))
+
+(define (world-change-dir w d)
+  (define the-snake (pit-snake w))
+  (cond [(and (opposite-dir? (snake-direction the-snake) d)
+              (cons? (rest (snake-segments the-snake))))
+         (stop-with w)]
+         [else
+          (pit (snake-change-dir the-snake d) (pit-goos w))]))
+
+;; snake funcs
 (define (snake-head sn)
   (first (snake-segments sn)))
 
@@ -46,10 +69,16 @@
 (define (snake-change-dir sn d)
   (snake d (snake-segments sn)))
 
+;; point funcs
 (define (point=? p1 p2)
   (and (= (point-x p1) (point-x p2))
        (= (point-y p1) (point-y p2))))
 
+(define (point-move p dx dy)
+  (point (+ (point-x p) dx)
+         (+ (point-y p) dy)))
+
+;; goo funcs
 (define (fresh-goo)
   (goo (point (+ 1 (random (sub1 SIZE)))
               (+ 1 (random (sub1 SIZE))))
@@ -75,10 +104,7 @@
 (define (age-goo goos)
   (rot (renew goos)))
 
-(define (point-move p dx dy)
-  (point (+ (point-x p) dx)
-         (+ (point-y p) dy)))
-
+;; misc funcs
 (define (next-head sn)
   (define head (snake-head sn))
   (define direction (snake-direction sn))
@@ -103,13 +129,26 @@
   (cons (fresh-goo) (remove goo-to-eat goos)))
 
 (define (close? snake goo)
-  (point=? snake (goo-location)))
+  (point=? snake (goo-location goo)))
 
 (define (can-eat sn goos)
   (cond [(empty? goos) #f]
         [else (if (close? (snake-head sn) (first goos))
                   (first goos)
                   (can-eat sn (rest goos)))]))
+
+(define (self-colliding? snake)
+  (cons? (member (snake-head snake) (snake-body snake))))
+
+(define (wall-colliding? snake)
+  (define x (point-x (snake-head snake))) 
+  (define y (point-y (snake-head snake))) 
+  (or (= 0 x) (= x SIZE)
+      (= 0 y) (= y SIZE)))
+
+(define (dead? w)
+  (define snake (pit-snake w))
+  (or (self-colliding? snake) (wall-colliding? snake)))
 
 ;; game stepping function
 (define (next-pit w)
@@ -119,6 +158,48 @@
   (if goo-to-eat
       (pit (grow snake) (age-goo (eat goos goo-to-eat)))
       (pit (slither snake) (age-goo goos))))
+
+
+;; rendering
+(define (render-end w)
+      (overlay (text "Game Over" ENDGAME-TEXT-SIZE "black")
+               (render-pit w)))
+
+(define (goo-list+scene goos scene)
+  (define (get-points-from-goo goos)
+    (cond [(empty? goos) empty]
+          [else (cons (goo-location (first goos))
+                      (get-points-from-goo (rest goos)))]))
+  (img-list+scene (get-points-from-goo goos) GOO-IMG scene))
+
+(define (img+scene point img scene)
+  (place-image img
+               (* (point-x point) SEG-SIZE)
+               (* (point-y point) SEG-SIZE)
+               scene))
+
+(define (img-list+scene points img scene)
+  (cond [(empty? points) scene]
+        [else (img+scene
+               (first points)
+               img
+               (img-list+scene (rest points) img scene))]))
+
+(define (snake+scene snake scene)
+      (define snake-body-scene
+        (img-list+scene  (snake-body snake) SEG-IMG scene))
+      (define dir (snake-direction snake))
+      (img+scene (snake-head snake)
+                 (cond [(string=? "up" dir) HEAD-UP-IMG]
+                       [(string=? "down" dir) HEAD-DOWN-IMG]
+                       [(string=? "left" dir) HEAD-LEFT-IMG]
+                       [(string=? "right" dir) HEAD-RIGHT-IMG])
+                 snake-body-scene))
+
+(define (render-pit w)
+      (snake+scene (pit-snake w)
+                   (goo-list+scene (pit-goos w) MT-SCENE)))
+
 
 ;; main function
 (define (start-snake)
