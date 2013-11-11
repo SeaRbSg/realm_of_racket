6c0f88ba8374d6a77c3bc298a0277dd9daa295ce
Ryan Davis <ryand-ruby@zenspider.com>
2013-11-11 15:06:59 -0800

    ch13 client networking code and logic

--- zenspider/ch13/client.rkt
+++ zenspider/ch13/client.rkt
@@ -1,3 +1,25 @@
 #lang racket
 
 (define clientstate0 "none available")
+
+(define (launch-guess-client n)
+  (big-bang clientstate0
+            (to-draw draw-guess)
+            (on-key handle-keys)
+            (name n)
+            (register LOCALHOST)
+            (on-receive handle-msg)))
+
+(define (handle-msg c msg)
+  (number->string msg))
+
+(define (handle-keys w key)
+  (case (string->symbol key)
+    [(up)   (make-package w "up")]
+    [(down) (make-package w "down")]
+    [(q)    (stop-with w)]
+    ((=)    (stop-with w))
+    [else   w]))
+
+(define (draw-guess c)
+  (overlay (text c SIZE COLOR) MT-SC))
