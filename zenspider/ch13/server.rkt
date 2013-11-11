3fc569bb19b4f0c6d9bdd9e09bb2c02e02b98186
Ryan Davis <ryand-ruby@zenspider.com>
2013-11-11 14:52:28 -0800

    server networking code and logic

--- zenspider/ch13/server.rkt
+++ zenspider/ch13/server.rkt
@@ -2,3 +2,41 @@
 
 (struct interval (small big))
 (define u0 (interval LOWER UPPER))
+
+(define (launch-guess-server)
+  (universe false
+            (on-new connect)
+            (on-msg handle-msg)))
+
+(define (connect u client)
+  (if (false? u)
+      (make-bundle u0 (list (make-mail client (guess o0))) empty)
+      (make-bundle u empty (list client))))
+
+(define (handle-msg u client msg)
+  (define (w (next-interval u msg)))
+  (make-bundle w (list (make-mail client (guess w))) empty))
+
+(define (next-interval u msg)
+  (cond [(not (string? msg)) u]
+        [(string=? "up" msg) (bigger u)]
+        [(string=? "down" msg) (smaller u)]
+        [else u]))
+
+;;; game logic:
+
+(define (bigger w)
+  (interval (min (interval-big w) (add1 (guess w)))
+            (interval-big w)
+            (add1 (interval-guesses w))))
+
+(define (guess w)
+  (quotient (+ (interval-small w) (interval-big w)) 2))
+
+(define (single? w)
+  (= (interval-small w) (interval-big w)))
+
+(define (smaller w)
+  (interval (interval-small w)
+            (max (interval-small w) (sub1 (guess w)))
+            (add1 (interval-guesses w))))
